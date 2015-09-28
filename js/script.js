aff08a071877331af55c5d1decf3ae167ce0b015
johnrdixon <johnryandixon@gmail.com>
2015-09-28 14:20:24 -0400

    Can't get jquery to work

--- js/script.js
+++ js/script.js
@@ -67,3 +67,53 @@ Dropdown.prototype.hide = function(){
 var dd = new Dropdown( "dropdown_button", "dropdown_menu" );
 
 dd. hide();
+
+
+( function( $ ) {
+$( document ).ready(function() {
+// Cache the elements we'll need
+var menu = $('#dropdown_menu');
+var menuList = menu.find('ul:first');
+var listItems = menu.find('li').not('#dropdown_button');
+
+// Create responsive trigger
+// menuList.prepend('<li id="dropdown_button"><a href="#">Menu</a></li>');
+
+// Toggle menu visibility
+menu.on('click', '#dropdown_button', function(){
+	listItems.slideToggle('fast');
+	listItems.addClass('collapsed');
+});
+});
+} )( jQuery );
+
+
+$(document).ready(function(){
+
+	// Custom jQuery code goes here
+    $('#cssmenu2 > ul > li:has(ul)').addClass("has-sub");
+    $('#cssmenu2 > ul > li > a').click(function() {
+
+	var checkElement = $(this).next();
+
+	$('#cssmenu2 li').removeClass('active');
+	$(this).closest('li').addClass('active');	
+
+	if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
+		$(this).closest('li').removeClass('active');
+		checkElement.slideUp('normal');
+	}
+
+	if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
+		$('#cssmenu2 ul ul:visible').slideUp('normal');
+		checkElement.slideDown('normal');
+	}
+
+	if (checkElement.is('ul')) {
+		return false;
+	} else {
+		return true;	
+	}
+});
+
+});
